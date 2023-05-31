ad514674d05796b2baf11cfef3988a455b1b2817
Leonardorossato <leonardorossato21@gmail.com>
2023-05-31 14:19:19 -0300

    update app.module.ts import polls file

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { PollsModule } from './polls/polls.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true })],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), PollsModule],
 })
 export class AppModule {}
