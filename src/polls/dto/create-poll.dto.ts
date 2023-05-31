27257476ff3403c22bca3c5b6cf58f78a05a76b0
Leonardorossato <leonardorossato21@gmail.com>
2023-05-31 14:43:24 -0300

    update create-poll.dto.ts

--- src/polls/dto/create-poll.dto.ts
+++ src/polls/dto/create-poll.dto.ts
@@ -1,1 +1,24 @@
-export class CreatePollDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { IsInt, Max, Min } from 'class-validator';
+
+export class CreatePollDto {
+  @ApiProperty()
+  topic: string;
+
+  @ApiProperty()
+  @IsInt()
+  @Min(1)
+  @Max(5)
+  votesPerVoter: number;
+
+  @ApiProperty()
+  name: string;
+}
+
+export class JoinPollDto {
+  @ApiProperty({ minLength: 6, maxLength: 6 })
+  polId: string;
+
+  @ApiProperty({ minLength: 1, maxLength: 18 })
+  name: string;
+}
