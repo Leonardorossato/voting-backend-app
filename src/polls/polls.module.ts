6bd52c9cc62858c66d06f8f4b19b86cce919605e
Leonardorossato <leonardorossato21@gmail.com>
2023-06-02 10:30:30 -0300

    update polls.module.ts

--- src/polls/polls.module.ts
+++ src/polls/polls.module.ts
@@ -2,10 +2,13 @@ import { Module } from '@nestjs/common';
 import { PollsService } from './polls.service';
 import { PollsController } from './polls.controller';
 import { RedisModule } from '../redis/redis.module';
+import { PollRepository } from './repository/poll.repository';
+import { ConfigModule } from '@nestjs/config';
+import { redisModule } from 'src/config/module.config';
 
 @Module({
-  imports: [RedisModule],
+  imports: [RedisModule, redisModule],
   controllers: [PollsController],
-  providers: [PollsService],
+  providers: [PollsService, PollRepository],
 })
 export class PollsModule {}
