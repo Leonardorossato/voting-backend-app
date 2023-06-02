2379f27445fb87c017422572b9da0bd9b14583bd
Leonardorossato <leonardorossato21@gmail.com>
2023-06-02 10:30:28 -0300

    update polls.service.ts

--- src/polls/polls.service.ts
+++ src/polls/polls.service.ts
@@ -1,22 +1,27 @@
-import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable, Logger } from '@nestjs/common';
 import {
   CreatePollField,
   JoinPollField,
   RejoinPollField,
 } from 'src/types/types';
 import { createPollID, createUserID } from 'src/utils/utils';
+import { PollRepository } from './repository/poll.repository';
 
 @Injectable()
 export class PollsService {
-  create(fields: CreatePollField) {
+  private readonly logger = new Logger(PollsService.name);
+  constructor(private readonly pollRepository: PollRepository) {}
+
+  async create(fields: CreatePollField) {
     try {
       const pollId = createPollID();
       const userId = createUserID();
-      return {
+      const createPoll = await this.pollRepository.createPoll({
         ...fields,
-        userId,
         pollId,
-      };
+        userId,
+      });
+      return createPoll;
     } catch (error) {
       throw new HttpException('Error creating poll', HttpStatus.BAD_REQUEST);
     }
@@ -25,16 +30,26 @@ export class PollsService {
   async joinPoll(dto: JoinPollField) {
     try {
       const userId = createUserID();
-      return {
-        ...dto,
-        userId,
-      };
+      this.logger.debug(
+        `Fetching with id:${dto.pollId} for user with id: ${userId}`,
+      );
+      const joinPoll = await this.pollRepository.getPoll(dto.pollId);
+      return joinPoll;
     } catch (error) {
       throw new HttpException('Error joining poll', HttpStatus.BAD_REQUEST);
     }
   }
 
   async rejoinPoll(fields: RejoinPollField) {
-    return fields;
+    try {
+      this.logger.debug(
+        `Rejoining with id:${fields.pollId} for user with id: ${fields.userId} with name: ${fields.name}`,
+      );
+
+      const joinedPoll = await this.pollRepository.addParticipant(fields);
+      return joinedPoll;
+    } catch (error) {
+      throw new HttpException('Error joining poll', HttpStatus.BAD_REQUEST);
+    }
   }
 }
