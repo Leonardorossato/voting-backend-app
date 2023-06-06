6a22e2f78119185d49d0ec345f90137fe53242ab
Leonardorossato <leonardorossato21@gmail.com>
2023-06-06 18:25:20 -0300

    update polls.gateway.ts

--- src/polls/polls.gateway.ts
+++ src/polls/polls.gateway.ts
@@ -1,13 +1,28 @@
-import { OnGatewayInit, WebSocketGateway } from '@nestjs/websockets';
+import {
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  OnGatewayInit,
+  WebSocketGateway,
+} from '@nestjs/websockets';
 import { PollsService } from './polls.service';
 import { Logger } from '@nestjs/common';
 
 @WebSocketGateway({
   namespace: 'polls',
 })
-export class PollsGateway implements OnGatewayInit {
+export class PollsGateway
+  implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect
+{
   private readonly logger = new Logger(PollsGateway.name);
   constructor(private readonly pollsService: PollsService) {}
+
+  handleConnection(client: any) {
+    throw new Error('Method not implemented');
+  }
+
+  handleDisconnect(client: any) {
+    throw new Error('Method not implemented');
+  }
 
   afterInit() {
     this.logger.log(`Websocket Gateway initialized.`);
