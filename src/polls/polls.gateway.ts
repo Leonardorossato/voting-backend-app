357010aa078b700479575e108e10d220194513c5
Leonardorossato <leonardorossato21@gmail.com>
2023-06-06 18:38:10 -0300

    update polls.gateway.ts

--- src/polls/polls.gateway.ts
+++ src/polls/polls.gateway.ts
@@ -3,9 +3,11 @@ import {
   OnGatewayDisconnect,
   OnGatewayInit,
   WebSocketGateway,
+  WebSocketServer,
 } from '@nestjs/websockets';
 import { PollsService } from './polls.service';
 import { Logger } from '@nestjs/common';
+import { Namespace, Socket } from 'socket.io';
 
 @WebSocketGateway({
   namespace: 'polls',
@@ -16,15 +18,29 @@ export class PollsGateway
   private readonly logger = new Logger(PollsGateway.name);
   constructor(private readonly pollsService: PollsService) {}
 
-  handleConnection(client: any) {
-    throw new Error('Method not implemented');
-  }
-
-  handleDisconnect(client: any) {
-    throw new Error('Method not implemented');
-  }
+  @WebSocketServer() io: Namespace;
 
   afterInit() {
     this.logger.log(`Websocket Gateway initialized.`);
+  }
+
+  handleConnection(client: Socket) {
+    try {
+      const sockets = this.io.sockets;
+      this.logger.log(`WS Client with ${client.id} connected.`);
+      this.logger.debug(`Number of connected sockets: ${sockets.size}`);
+    } catch (error) {
+      throw new Error('Method not implemented');
+    }
+  }
+
+  handleDisconnect(client: Socket) {
+    try {
+      const sockets = this.io.sockets;
+      this.logger.log(`Disconneted docket: ${client.id}.`);
+      this.logger.debug(`Number of disconneted sockets: ${sockets.size}`);
+    } catch (error) {
+      throw new Error('Method not implemented');
+    }
   }
 }
