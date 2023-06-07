0d889d2f4b1eca894ca5e6f52ca1e976e395f3c1
Leonardorossato <leonardorossato21@gmail.com>
2023-06-07 10:36:58 -0300

    update socket.io.types.ts

--- src/types/socket.io.types.ts
+++ src/types/socket.io.types.ts
@@ -1,6 +1,8 @@
 import { INestApplicationContext, Logger } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import { IoAdapter } from '@nestjs/platform-socket.io';
-import { ServerOptions } from 'socket.io';
+import { Server, ServerOptions } from 'socket.io';
+import { SocketWithAuth } from './types';
 
 export class SocketIoTypes extends IoAdapter {
   private readonly logger = new Logger(SocketIoTypes.name);
@@ -22,6 +24,27 @@ export class SocketIoTypes extends IoAdapter {
       ...options,
       cors,
     };
-    return super.createIOServer(port, optionsWithCors);
+
+    const jwtService = this.app.get(JwtService);
+    const server: Server = super.createIOServer(port, optionsWithCors);
+    server.of('polls').use(createTokenMiddleware(jwtService, this.logger));
+    return server;
   }
 }
+
+const createTokenMiddleware =
+  (jwtSersice: JwtService, logger: Logger) =>
+  (socket: SocketWithAuth, next) => {
+    try {
+      const token =
+        socket.handshake.auth.token || socket.handshake.headers['token'];
+      logger.debug(`Validating auth token: ${token}`);
+      const payload = jwtSersice.verify(token);
+      socket.userId = payload.sub;
+      socket.pollId = payload.pollId;
+      socket.name = payload.name;
+      next();
+    } catch (error) {
+      throw new Error('Forbidden token');
+    }
+  };
