61714bb43f0fccd8af3371864cb6c491ada65775
Leonardorossato <leonardorossato21@gmail.com>
2023-06-14 10:08:55 -0300

    update polls.gateway.ts

--- src/polls/polls.gateway.ts
+++ src/polls/polls.gateway.ts
@@ -2,6 +2,7 @@ import {
   OnGatewayConnection,
   OnGatewayDisconnect,
   OnGatewayInit,
+  SubscribeMessage,
   WebSocketGateway,
   WebSocketServer,
 } from '@nestjs/websockets';
@@ -26,7 +27,7 @@ export class PollsGateway
     this.logger.log(`Websocket Gateway initialized.`);
   }
 
-  handleConnection(client: SocketWithAuth) {
+  async handleConnection(client: SocketWithAuth) {
     try {
       const sockets = this.io.sockets;
       this.logger.debug(
@@ -34,21 +35,50 @@ export class PollsGateway
       );
       this.logger.log(`WS Client with ${client.id} connected.`);
       this.logger.debug(`Number of connected sockets: ${sockets.size}`);
+      const roomName = client.pollId;
+      await client.join(roomName);
+      const connectedClients = this.io.adapter.rooms.get(roomName).size ?? 0;
+      this.logger.debug(
+        `userId: ${client.userId} joined in room with name: ${roomName}`,
+      );
+      this.logger.debug(
+        `Total clients connectd to this room : ${roomName} : ${connectedClients}`,
+      );
+      const updatedPoll = await this.pollsService.addParticipant({
+        pollId: client.pollId,
+        userId: client.userId,
+        name: client.name,
+      });
+      return this.io.to(roomName).emit('poll_updated', updatedPoll);
     } catch (error) {
       throw new Error('Method not implemented');
     }
   }
 
-  handleDisconnect(client: SocketWithAuth) {
+  async handleDisconnect(client: SocketWithAuth) {
     try {
-      const sockets = this.io.sockets;
-      this.logger.debug(
-        `Socket connected with userId: ${client.userId}, pollId: ${client.pollId} and name: ${client.name}`,
+      const { pollId, userId } = client;
+      const updatedPoll = await this.pollsService.removeParticipant(
+        pollId,
+        userId,
       );
-      this.logger.log(`Disconneted docket: ${client.id}.`);
-      this.logger.debug(`Number of disconneted sockets: ${sockets.size}`);
+      const roomName = client.pollId;
+      const clientCount = this.io.adapter.rooms.get(roomName).size ?? 0;
+      this.logger.log(`Disconnected socket io: ${client.userId}`);
+      this.logger.log(`Number of connected sockets: ${clientCount}.`);
+      this.logger.debug(
+        `Total of clients connected to this room: ${roomName}: ${clientCount}`,
+      );
+      if (updatedPoll) {
+        this.io.to(pollId).emit('poll_updated', updatedPoll);
+      }
     } catch (error) {
       throw new Error('Method not implemented');
     }
+  }
+
+  @SubscribeMessage('test')
+  async test() {
+    throw new Error('plain operation');
   }
 }
